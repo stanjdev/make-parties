7892eb217416e2cc80b016c43755ebf4f9e3b6bb
Stanley Jeong <stanley.jeong@shopify.com>
2022-04-01 12:29:05 -0400

    users can delete rsvps

--- controllers/rsvps.js
+++ controllers/rsvps.js
@@ -19,4 +19,12 @@ module.exports = (app, models) => {
   });
 
   // DESTROY  
+  app.delete('/events/:eventId/rsvps/:id', (req, res) => {
+    models.Rsvp.findByPk(req.params.id).then((rsvp) => {
+      rsvp.destroy();
+      res.redirect(`/events/${req.params.eventId}`);
+    }).catch((err) => {
+      console.log(err);
+    });
+  });
 }
